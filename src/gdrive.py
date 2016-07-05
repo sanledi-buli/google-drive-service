05d400392476b3965b7e3fe8cd4f4c6d880e627f
sanledi buli <sanledi.buli@gmail.com>
2016-07-06 02:39:36 +0700

    Fixed get by file ID and change file permission

--- src/gdrive.py
+++ src/gdrive.py
@@ -11,3 +11,10 @@ class GoogleDrive(object):
         results = self.__service_v3.files().list().execute()
         items = results.get('files', [])
         return items
+
+    def get_file_by_id(self,file_id):
+        result = self.__service_v2.files().get(fileId=file_id).execute()
+        return result
+
+    def set_permissions(self, file_id, permissions = {}):
+        self.__service_v2.permissions().insert(fileId=file_id, body=permissions).execute()
